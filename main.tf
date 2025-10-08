0232856e352a166f8f362f0df6a69872a7a2b7ae
LioriFR23 <lior@firefly.ai>
2025-10-08 12:31:06 +0300

    test

--- main.tf
+++ main.tf
@@ -6,8 +6,3 @@ resource "aws_s3_object" "demo_text" {
   content = "hello Firefly!"
 }
 
-resource "aws_s3_object" "demo_text1" {
-  bucket  = "firefly-tfstate-runner-350715735192"
-  key     = "test_workflow/demo1.txt"
-  content = "hello Firefly!"
-}
